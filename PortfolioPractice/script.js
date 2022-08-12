2b1513bcecb6a0a0f1191bbfff9f4a09b4f1afc8
Berlin <Berline_lucien@yahoo.com>
2022-08-12 14:15:50 -0400

    fixed console log submit function

--- PortfolioPractice/script.js
+++ PortfolioPractice/script.js
@@ -1,40 +1,12 @@
+
+
+
 /** FORM Function when client hit submit */
 
-
-
-const userName = document.getElementById("name");
-document.querySelector("form").addEventListener('submit',function(e){
+form.addEventListener("submit", (e) => {
     e.preventDefault();
-    userName.required = true;
-    console.log(userName.value);
-});
+    let username = document.getElementById("username").value
 
-// form.addEventListener("submit", (e) => {
-//     e.preventDefault();
-//     let userName = document.getElementById("name").value;
-//     console.log(userName);
-    
-//     });
-    
-
-// Example starter JavaScript for disabling form submissions if there are invalid fields
-(() => {
-    'use strict'
+    console.log(username);
   
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    const forms = document.querySelectorAll('.needs-validation')
-  
-    // Loop over them and prevent submission
-    Array.from(forms).forEach(form => {
-      form.addEventListener('submit', event => {
-        if (!form.checkValidity()) {
-          event.preventDefault()
-          event.stopPropagation()
-          
-        }
-  
-        form.classList.add('was-validated')
-      }, false)
-    
-    })
-  })()
+});  
