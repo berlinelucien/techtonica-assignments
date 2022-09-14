5609c79e5f22ace6977756402d7d7a2844d25df4
Berlin <Berline_lucien@yahoo.com>
2022-09-14 07:14:34 -0400

    front end and backend connected ... kinddaaa

---
+++ Quiz-app/client/src/App.js
@@ -0,0 +1,58 @@
+import './App.css';
+//import axios from 'axios';
+import { useState, useEffect } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from "./Pages/Home/Home";
+import Quiz from "./Pages/Quiz/Quiz";
+import Result from "./Pages/Result/Result";
+
+function App() {
+  const [questions, setQuestions] = useState();
+  const [name, setName] = useState();
+  const [score, setScore] = useState(0);
+
+  const fetchQuestions = () => {
+    fetch('http://localhost:8080/game')
+      .then((response) => response.json())
+      .then(data => {
+        console.log("This is line 11", data.results);
+        setQuestions(data.results);
+      })
+  }
+useEffect(() => {
+  fetchQuestions();
+}, [])
+
+
+  return (
+    <BrowserRouter>
+      <div className="app" style={{ backgroundImage: 'url("/ques1.png")' }}>
+   
+        <Switch>
+          <Route path="/" exact>
+            <Home
+              name={name}
+              setName={setName}
+              fetchQuestions={fetchQuestions}
+            />
+          </Route>
+          <Route path="/quiz">
+            <Quiz
+              name={name}
+              questions={questions}
+              score={score}
+              setScore={setScore}
+              setQuestions={setQuestions}
+            />
+          </Route>
+          <Route path="/result">
+            <Result name={name} score={score} />
+          </Route>
+        </Switch>
+      </div>
+  
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ Quiz-app/server/index.js
@@ -0,0 +1,43 @@
+import express from "express";
+import cors from "cors";
+//import axios from "axios"
+import bodyParser from 'body-parser';
+import fetch from 'node-fetch';
+import * as dotenv from "dotenv"
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+app.use(cors());
+// Configuring body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+
+app.get('/', (req, res) => {
+    res.json({message: "Bonjour "})
+})
+// `https://opentdb.com/api.php?amount=10&category=31&type=boolean`
+// GET request for the GAME api
+app.get('/game', cors(), async (req, res) => {
+    const url = `https://opentdb.com/api.php?amount=10&category=12&type=multiple`;
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        //console.log("Line 27 server.js", data);
+        res.send(data);
+      } catch (err) {
+        console.error("Fetch error: ", err);
+      }
+});
+
+
+
+
+
+
+// console.log that your server is up and running
+app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+  });
