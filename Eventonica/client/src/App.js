7a8586b8fbaa87bc181e8fe2390ade581b32529a
Berlin <Berline_lucien@yahoo.com>
2022-09-19 19:25:16 -0400

    event file created and hardcode event information

--- Eventonica/client/src/App.js
+++ Eventonica/client/src/App.js
@@ -2,39 +2,20 @@ import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Users from './components/Users';
+import Event from './components/Event';
+
+
 
 function App() {
   return (
     <div className="App">
+       {/** header component here */}
      <Header />
       <main>
         <div className="user-and-events">
-         <Users />
-
-          <section className="event-management">
-            <h2>Event Management</h2>
-            <div>
-              <h3>All Events</h3>
-              <ul id="events-list">
-                {/* Display all Events here */}
-                <li>...</li>
-              </ul>
-
-              <h3>Add Event</h3>
-              <form id="add-event" action="#">
-                <fieldset>
-                  <label>Name</label>
-                  <input
-                    type="text"
-                    id="add-event-name"
-                    placeholder="Virtual corgi meetup"
-                  />
-                </fieldset>
-                {/* Add more form fields here */}
-                <input type="submit" />
-              </form>
-            </div>
-          </section>
+          {/**users and event component here */}
+          <Users />
+          <Event />
         </div>
 
         <div>
