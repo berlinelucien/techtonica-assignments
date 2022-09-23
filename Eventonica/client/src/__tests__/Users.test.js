474cdd668a4a44f9e961920e6b3067ea930f7486
Berlin <Berline_lucien@yahoo.com>
2022-09-22 20:02:37 -0400

    react testing added to client

---
+++ Eventonica/client/src/__tests__/Users.test.js
@@ -0,0 +1,11 @@
+import { render, screen } from '@testing-library/react';
+import Users from '../components/Users'
+
+
+describe("Events", () => {
+    test("should render Events component", () => {
+      render(<Users />);
+  
+    });
+});
+  
--- Eventonica/client/src/components/EventForm.jsx
+++ Eventonica/client/src/components/EventForm.jsx
@@ -5,26 +5,26 @@ import AddNewEvent from "./AddEvent";
 import FindEvent from "./FindEvent";
 import Header from "./Header";
 import { MdDelete } from "react-icons/md";
-import Checkbox from '@mui/material/Checkbox';
-import FavoriteBorder from '@mui/icons-material/FavoriteBorder';
-import Favorite from '@mui/icons-material/Favorite';
+import Checkbox from "@mui/material/Checkbox";
+import FavoriteBorder from "@mui/icons-material/FavoriteBorder";
+import Favorite from "@mui/icons-material/Favorite";
 import SearchIcon from "@mui/icons-material/Search";
 
-const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
+const label = { inputProps: { "aria-label": "Checkbox demo" } };
 
 const EventForm = () => {
   // setting the data
-  const [events, setEvents] = useState([])
+  const [events, setEvents] = useState([]);
   console.log("events", events);
 
-   //     set search query to empty string
-   const [q, setQ] = useState("");
-   //     set search parameters
-   //     we only what to search countries by capital and name
-   //     this list can be longer if you want
-   //     you can search countries even by their population
-   // just add it to this array
-   const [searchParam] = useState(["id", "name"]);
+  //     set search query to empty string
+  const [q, setQ] = useState("");
+  //     set search parameters
+  //     we only what to search countries by capital and name
+  //     this list can be longer if you want
+  //     you can search countries even by their population
+  // just add it to this array
+  const [searchParam] = useState(["id", "name"]);
 
   //const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
   // connect backend to frontend
@@ -35,20 +35,21 @@ const EventForm = () => {
     setEvents(event);
   };
   useEffect(() => {
-    // useEffect will run getEvents() 
+    // useEffect will run getEvents()
     getEvents();
   }, []);
-  
+
+  //POST
   // add events
   const handleAddEvent = async (newEvents) => {
-    const response = await fetch('http://localhost:4000/events', {
-      method: 'POST',
+    const response = await fetch("http://localhost:4000/events", {
+      method: "POST",
       headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json'
+        Accept: "application/json",
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(newEvents)
-  });
+      body: JSON.stringify(newEvents),
+    });
     const content = await response.json();
     setEvents(events);
     setEvents([...events, content]);
@@ -57,7 +58,9 @@ const EventForm = () => {
     //     setEvents(events);
     // setEvents([...events, newEvents]);
   };
+  //DELETE
   // delete events prop/function from delete event page
+  // delete connected to backend/DB
   const deleteEvent = async (deleteId) => {
     let response = await fetch(`http://localhost:4000/events/${deleteId}`, {
       method: "DELETE",
@@ -66,8 +69,27 @@ const EventForm = () => {
     const newEvents = events.filter((event) => event.id !== deleteId);
     setEvents(newEvents);
   };
+  //PUT
+  // UPDATE EVENTS
+  // const updateEvents = async (newEvent) => {
+  //   let response = fetch('http://localhost:4000/events', {
+  //     method: 'PUT',
+  //     headers: {
+  //         Accept: 'application/json',
+  //         'Content-Type': 'application/json'
+  //     },
+  //     body: JSON.stringify(newEvent)
+  //   });
+  //   const content = await response.json();
+  //   events.forEach((event) => {
+  //     updateEvents = {
+  //       ...event,
 
-  // search events
+  //     }
+  //   })
+  // }
+
+  // SEARCH EVENTS
   // input will be converted to string, toLowercase, only looking
   // for parameters q= name
   const searchItem = (events) => {
@@ -87,70 +109,70 @@ const EventForm = () => {
           <Header text="Event Management" />{" "}
         </h2>
         <div>
-         
           <div className="search">
-        <label htmlFor="search-form">
-          <input
-            type="search"
-            name="search-form"
-            id="search-form"
-            className="search-input"
-            placeholder="Search for event"
-            value={q}
-            /*
+            <label htmlFor="search-form">
+              <input
+                type="search"
+                name="search-form"
+                id="search-form"
+                className="search-input"
+                placeholder="Search for event"
+                value={q}
+                /*
           // set the value of our useState q
           //  anytime the user types in the search box
           */
-            onChange={(e) => setQ(e.target.value)}
-          />
-          <span className="searchIcon">
-            <SearchIcon />
-          </span>
-        </label>
+                onChange={(e) => setQ(e.target.value)}
+              />
+              <span className="searchIcon">
+                <SearchIcon />
+              </span>
+            </label>
           </div>
           <div className="container">
-          <h3>All Events</h3>
-          <ul id="events-list">
-            <table className="table table-hover border">
-              <thead className="thead-dark">
-                <tr>
-                  <th scope="col">#:</th>
-                  <th scope="col">Event:</th>
-                  <th scope="col">Date:</th>
-                  <th scope="col">Description:</th>
-                  <th scope="col">Category:</th>
-                  <th scope="col"></th>
-                </tr>
-              </thead>
-              <tbody>
-                {/* Display all Events here */}
-                {searchItem(events).map((event, index) => {
-                  return (
-                    <tr key={index}>
-                      <th scope="row">{event?.id}</th>
-                      <td>{event?.name}</td>
-                      <td>{event?.date}</td>
-                      <td>{event?.description}</td>
-                      <td>{event?.category}</td>
-                      <td>
-                        <MdDelete onClick={() => deleteEvent(event.id)} />
-                        <Checkbox {...label}
-                          color="secondary"
-                          icon={<FavoriteBorder />}
-                          checkedIcon={<Favorite />} />
-                      </td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </table>
+            <h3>All Events</h3>
+            <ul id="events-list">
+              <table className="table table-hover border">
+                <thead className="thead-dark">
+                  <tr>
+                    <th scope="col">#:</th>
+                    <th scope="col">Event:</th>
+                    <th scope="col">Date:</th>
+                    <th scope="col">Description:</th>
+                    <th scope="col">Category:</th>
+                    <th scope="col"></th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {/* Display all Events here */}
+                  {searchItem(events).map((event, index) => {
+                    return (
+                      <tr key={index}>
+                        <th scope="row">{event?.id}</th>
+                        <td>{event?.name}</td>
+                        <td>{event?.date}</td>
+                        <td>{event?.description}</td>
+                        <td>{event?.category}</td>
+                        <td>
+                          <MdDelete onClick={() => deleteEvent(event.id)} />
+                          <Checkbox
+                            {...label}
+                            color="secondary"
+                            icon={<FavoriteBorder />}
+                            checkedIcon={<Favorite />}
+                          />
+                        </td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
             </ul>
-            </div>
+          </div>
           {/** add event form */}
           <div className="addUserArea">
-          <AddNewEvent handleAddEvent={handleAddEvent} />
+            <AddNewEvent handleAddEvent={handleAddEvent} />
           </div>
-          
         </div>
       </section>
 
